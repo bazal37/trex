fecacf8f512ab81d984335d993d4c41ed88b60fa
bazal37 <117945322+bazal37@users.noreply.github.com>
2023-01-02 17:26:19 -0500

    Add files via upload

---
+++ t-rex-0.26.8-linux/ETC-2miners.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a etchash -o stratum+tcp://daggerhashimoto.eu-west.nicehash.com:9200 -u NHbavxKhSvccjtaoaQacRAqNGpF2Gte8upSC.liman -p x -w rig0
---
+++ t-rex-0.26.8-linux/RVN-2miners.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a KAWPOW -o stratum+tcp://kawpow.eu-west.nicehash.com:9200 -u NHbavxKhSvccjtaoaQacRAqNGpF2Gte8upSC.rig -p x
